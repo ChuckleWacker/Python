79e053918ff57dad5e55366f796e7765987227a8
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-07-09 23:25:15 -0700

    Update Week9_TomeRater_Project.py

--- Week9_TomeRater_Project.py
+++ Week9_TomeRater_Project.py
@@ -13,9 +13,7 @@ class User:  # Methods Tested
         self.email = address
         print("This user's email has been updated")  # Should also print a message saying the users email was updated
 
-    def read_book(self, book, rating):  # Page5 ###################################################################
-        if rating == "":
-            rating = "None"
+    def read_book(self, book, rating=None):
         self.books[book] = rating
 
     def get_average_rating(self):
@@ -27,19 +25,18 @@ class User:  # Methods Tested
                 total_ratings += rating
         return total_ratings / len(self.books)  # Divide total_ratings by the number of books in dictionary
 
-
     def __repr__(self):  # Returns a string to print out this user object in a meaningful way.
         return "User: {},  Email: {},  Books Read: {}.".format(self.name, self.email, len(self.books))
 
     def __eq__(self, other_user):  # Returns True if name & email match, else False
         return self.name == other_user.name and self.email == other_user.email
 # TESTS
-alan = User("Alan Turing", "alan@turing.com")
-dan = User("Daniel Boggs", "dan@email.com")
-alan.change_email("new@email.com")
-print(alan)
-print(alan.__eq__(dan))  # Prints False
-print(alan.__eq__(alan))  # Prints True
+#alan = User("Alan Turing", "alan@turing.com")
+#dan = User("Daniel Boggs", "dan@email.com")
+#alan.change_email("new@email.com")
+#print(alan)
+#print(alan.__eq__(dan))  # Prints False
+#print(alan.__eq__(alan))  # Prints True
 
 
 class Book:
@@ -47,6 +44,9 @@ class Book:
         self.title = title  # String
         self.isbn = isbn  # Number
         self.ratings = []  # Instance variable as empty list
+
+    def __repr__(self):
+        return "{}, ISBN: {}".format(self.title, self.isbn)
 
     def get_title(self):
         return self.title
@@ -87,10 +87,10 @@ class Fiction(Book):
 
     def __repr__(self):
         return "{} by {}".format(self.title, self.author)
-fict1 = Fiction("Alice In Wonderland", "Lewis Carroll", 12345)
-fict2 = Fiction("Chthulu", "HP Lovecraft", 112233)
-print(fict1)
-print(fict1.get_author())
+#fict1 = Fiction("Alice In Wonderland", "Lewis Carroll", 12345)
+#fict2 = Fiction("Chthulu", "HP Lovecraft", 112233)
+#print(fict1)
+#print(fict1.get_author())
 
 
 class Non_Fiction(Book):
@@ -107,19 +107,25 @@ class Non_Fiction(Book):
 
     def __repr__(self):
         return "{}, a {} manual on {}".format(self.title, self.level, self.subject)
-non_fict1 = Non_Fiction("Automate the Boring Stuff", "Python", "beginner", 1929452)
-non_fict2 = Non_Fiction("Computing Machinery and Intelligence", "AI", "advanced", 11111938)
-print(non_fict1)
-print(non_fict2)
-non_fict2.add_rating(2)
-print(non_fict2.ratings)  # Prints 2
-print(non_fict1.__eq__(non_fict1))  # Returns True
+#non_fict1 = Non_Fiction("Automate the Boring Stuff", "Python", "beginner", 1929452)
+#non_fict2 = Non_Fiction("Computing Machinery and Intelligence", "AI", "advanced", 11111938)
+#print(non_fict1)
+#print(non_fict2)
+#non_fict2.add_rating(2)
+#print(non_fict2.ratings)  # Prints 2
+#print(non_fict1.__eq__(non_fict1))  # Returns True
 
 
 class TomeRater:
     def __init__(self):
         self.users = {}
         self.books = {}
+
+    def __repr__(self):
+        return "{} users have read {} books".format(len(self.users), len(self.books))
+
+    def __eq__(self, other_object):  # Returns True if title match, else False
+        return self.title == other_object.title
 
     def create_book(self, title, isbn):
         book = Book(title, isbn)
@@ -133,28 +139,66 @@ class TomeRater:
         non_fiction = Non_Fiction(title, subject, level, isbn)
         return non_fiction
 
-    def add_book_to_user(self, book, email, rating):  # #########################################################
-        if len(rating) == 0:
-            rating = None
-        if
-
-
-    def add_user(self, name, email, books):
-        user = User(name, email)
-        if len(books) == 0:
-            books = None
+    def add_book_to_user(self, book, email, rating=None):
+        user = self.users.get(email, None)
+        if user:
+            user.read_book(book, rating)
+            if rating is not None:
+                book.add_rating(rating)
+            if book not in self.books:  # If book not in self.books, add to self.books with read count of 1
+                self.books[book] = 1
+            else:                       # Else, the book already exists in self.books so increase read count by 1
+                self.books[book] += 1
         else:
+            print("No user with email {}!".format(email))
+
+    def add_user(self, name, email, books=None):
+        user = User(name, email)
+        self.users[email] = user
+        if books is not None:
             for book in books:
-                self.add_book_to_user(book, email, )
+                self.add_book_to_user(book, email)
+
+    def print_catalog(self):
+        for book in self.books:
+            print(book)
+
+    def print_users(self):
+        for user in self.users:
+            print(user)
+
+    def most_read_book(self):  # Returns the highest value found in the dictionary
+        highest_value = 0
+        highest_key = ""
+        for book in self.books:
+            if self.books.get(book) > highest_value:
+                highest_value = self.books.get(book)
+                highest_key = book
+        return highest_key
+
+    def highest_rated_book(self):
+        pass
+
+    def most_positive_user(self):
+        pass
 
 
+print("Start")
+print("")
 
 test_book = TomeRater()
-print(test_book.create_book("Book", 1111))
-test_novel = TomeRater()
-print(test_novel.create_novel("Novel", "R.L. Stein", 2222))
-test_nonfiction = TomeRater()
-print(test_nonfiction.create_non_fiction("NonFiction", "Writing", "Hard", 3333))
+test_book.create_book("Book", 1111)
+test_book.add_book_to_user(Book, "daniel.a.boggs@gmail.com")
+test_book.add_user("Dan", "daniel.a.boggs@gmail.com")
+test_book.add_book_to_user(Book, "daniel.a.boggs@gmail.com")
+print(test_book)
+#print(test_book.print_catalog())
+#print(test_book.print_users())
 
 
+#test_novel = TomeRater()
+#print(test_novel.create_novel("Novel", "R.L. Stein", 2222))
+#test_nonfiction = TomeRater()
+#print(test_nonfiction.create_non_fiction("NonFiction", "Writing", "Hard", 3333))
+print("")
 print("End")
